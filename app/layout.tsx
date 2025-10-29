50836a6fa0f44babac97a635c3636eab3737de68
Jose Lopez Renteria <joselukas24@gmail.com>
2025-10-29 15:41:57 -0500

    got basic layout for desktop done

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,66 +6,70 @@ import "./globals.css";
 import Providers from "@/providers";
 
 const geistSans = Geist({
-	variable: "--font-geist-sans",
-	subsets: ["latin"],
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
 });
 
 const geistMono = Geist_Mono({
-	variable: "--font-geist-mono",
-	subsets: ["latin"],
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
 });
 
 const jetBrainsMono = JetBrains_Mono({
-	variable: "--font-jetbrains-mono",
+  variable: "--font-jetbrains-mono",
 });
 
 const berkeleyMono = localFont({
-	src: [
-		{
-			path: "./fonts/TX-02-Regular.otf",
-			weight: "400",
-			style: "normal",
-		},
-		{
-			path: "./fonts/TX-02-Bold.otf",
-			weight: "700",
-			style: "normal",
-		},
-		{
-			path: "./fonts/TX-02-Oblique.otf",
-			weight: "400",
-			style: "oblique",
-		},
+  src: [
+    {
+      path: "./fonts/TX-02-Regular.otf",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "./fonts/TX-02-Bold.otf",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "./fonts/TX-02-Oblique.otf",
+      weight: "400",
+      style: "oblique",
+    },
     {
       path: "./fonts/TX-02-Bold-Oblique.otf",
       weight: "700",
       style: "oblique",
     },
-	],
-	variable: "--font-berkeley",
-	display: "swap",
+  ],
+  variable: "--font-berkeley",
+  display: "swap",
 });
 
 export const metadata: Metadata = {
-	title: "WunderScout",
-	description: "Soccer Analytics",
+  title: "WunderScout",
+  description: "Soccer Analytics",
 };
 
 export default function RootLayout({
-	children,
+  children,
 }: Readonly<{
-	children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-	return (
-		<html lang="en" suppressHydrationWarning>
-			<body
-				className={`${geistSans.variable} ${geistMono.variable} ${jetBrainsMono.variable} ${berkeleyMono.variable} antialiased flex flex-col min-h-dvh`}
-			>
-				<Providers>
-					<header className="p-4 text-2xl text-gray-500 shrink-0">WunderScout Analytics</header>
-					<main className="border-2 border-green-500 flex-1 flex flex-col">{children}</main>
-				</Providers>
-			</body>
-		</html>
-	);
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} ${jetBrainsMono.variable} ${berkeleyMono.variable} antialiased flex flex-col min-h-dvh`}
+      >
+        <Providers>
+          <header className="p-4 text-2xl text-gray-500 shrink-0">
+            WunderScout Analytics
+          </header>
+          <main className="border-2 border-green-500 flex-1 flex">
+            {children}
+          </main>
+        </Providers>
+      </body>
+    </html>
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,23 @@
-
 export default function Page() {
   return (
-    <main className="p-2 flex-1 flex flex-row border-2 border-red-500">
-      // Desktop layout
-      <div>
-        <div className="border-2 border-amber-400 flex-2">
-          This is the left side.
+    <div className="p-2 flex-1 border-3 border-dashed border-red-500">
+      {/* Desktop Layout */}
+      <div className="hidden lg:grid lg:grid-cols-[30%_70%] h-full min-h-0 border-4 border-dotted border-emerald-800">
+        <div className="grid grid-rows-[auto_auto_1fr] border-2 border-amber-400">
+          <div className="border-1 border-dashed border-white">Item 1</div>
+          <div className="border-1 border-dashed border-white">Item 2</div>
+          <div className="border-1 border-dashed border-white">Item 3</div>
         </div>
-        <div className="border-2 border-blue-300 flex-5">
-          This is the right side.
+        <div className="grid grid-rows-[1fr_7fr_auto] border-2 border-blue-300">
+          <div className="border-1 border-dashed border-white">Item 1</div>
+          <div className="border-1 border-dashed border-white">Item 2</div>
+          <div className="border-1 border-dashed border-white">Item 3</div>
         </div>
       </div>
-    </main>
+      {/* Mobile Layout */}
+      <div className="lg:hidden h-full border-2 border-white">
+        <section>This is the only side.</section>
+      </div>
+    </div>
   );
 }
