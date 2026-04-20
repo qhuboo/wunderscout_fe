4a30700cbdb64466d318afcd7b57ebf24a725dbb
qhuboo <joselukas24@gmail.com>
2026-04-19 23:52:18 -0500

    working

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
-const nextConfig = {
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  transpilePackages: ["three"],
   turbopack: {
     rules: {
       "*.glsl": {
+        loaders: ["raw-loader"],
+        as: "*.js",
+      },
+      "*.vert": {
+        loaders: ["raw-loader"],
+        as: "*.js",
+      },
+      "*.frag": {
         loaders: ["raw-loader"],
         as: "*.js",
       },
